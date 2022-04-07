f23e007c86c1ee9c51e1e3822a4fd401e0d5e062
unknown <h45652@naver.com>
2022-04-07 14:33:08 +0900

    feat: admin 커스텀필드 & 오픈소스 어드민 스타일링

--- fc_django/fcuser/admin.py
+++ fc_django/fcuser/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from .models import Fcuser
 
+
 class FcuserAdmin(admin.ModelAdmin):
     list_display = ('email',)
 
+
 admin.site.register(Fcuser, FcuserAdmin)
+admin.site.site_header = '패스트캠퍼스'
+admin.site.index_title = '패스트 캠퍼스'
+admin.site.site_title = '패스트 캠퍼스'
--- fc_django/order/admin.py
+++ fc_django/order/admin.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
+from django.utils.html import format_html # 태그를 사용할 수 있게 해줌. 보통은 <b>test</b> 이렇게 출력됨
 from .models import Order
 
 
-class OrderAdmin(admin.ModelAdmin):
-    list_display = ('fcuser', 'product')
 
+class OrderAdmin(admin.ModelAdmin):
+    list_filter = ('status',)
+    list_display = ('fcuser', 'product', 'styled_status')
+
+    def styled_status(self, obj): # obj == 각 레코드를 의미
+        if obj.status == '환불':
+            return format_html(f'<span style="color:red">{obj.status}</span>')
+        if obj.status == '결제완료':
+            return format_html(f'<span style="color:green">{obj.status}</span>')
+        return format_html(f'<b>{obj.status}</b>')
+
+    styled_status.shor_description = '상태'
 
 admin.site.register(Order, OrderAdmin)
